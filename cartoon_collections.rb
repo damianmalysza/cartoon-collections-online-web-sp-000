0b54eb186e32c4c57520f2e7b306040352f5d1a3
Damian Malysza <damian.malysza@gmail.com>
2020-10-20 21:01:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(array)
   array.any? {|i| i.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
